9e2ebfc8ff307724ad0ff87cc1077a830848a312
Hippolyte Dreyfus <hippolytedreyfus@orange.fr>
2026-06-12 17:22:44 +0200

    refacto

--- simnibs_analyze/_outputs.py
+++ simnibs_analyze/_outputs.py
@@ -1,15 +1,15 @@
+# TODO
 
 
+# plus precisement mais tu peux deja ecrire des emptys methodes pour des save de format
 
-plus precisement mais tu peux deja ecrire des emptys methodes pour des save de format 
+# -utiliser les methodes reader pour les csv, les extractions
 
--utiliser les methodes reader pour les csv, les extractions
+# - niftii
+# (ou le format 3d niivue adapté au save de plsueiurs images colocalisée a aprtir duquel on crée les figures)
 
-- niftii 
-(ou le format 3d niivue adapté au save de plsueiurs images colocalisée a aprtir duquel on crée les figures) 
+# - plot
 
-- plot
+# ---cohort-plot
 
----cohort-plot
-
-- plot2dstats
+# - plot2dstats
